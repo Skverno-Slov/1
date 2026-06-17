4fd4fd73f738349b997e57154deac7180008dc2a
SkvernoSlov <zaharlukanin19@gmail.com>
2026-06-18 00:26:06 +0300

    lr23

---
+++ syspr/LabWork21/LabWork21/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.Win32;
+using System.Management;
+using System.Net.NetworkInformation;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace LabWork21
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            LoadWmiInfo("Win32_Processor", ProcessorList, ["Name", "NumberOfCores", "MaxClockSpeed"]);
+            LoadWmiInfo("Win32_VideoController", VideoList, ["Name", "AdapterRAM", "DriverVersion"]);
+            LoadWmiInfo("Win32_MotherboardDevice", MotherboardList, ["Product", "Manufacturer", "SerialNumber"]);
+            LoadWmiInfo("Win32_DiskDrive", DiskList, ["Model", "Size", "InterfaceType"]);
+            LoadWmiInfo("Win32_OperatingSystem", OsList, ["Caption", "Version", "OSArchitecture"]);
+
+            LoadNetworkInfo();
+
+            LoadInstalledPrograms();
+        }
+
+        private void LoadWmiInfo(string wmiClass, ListBox list, string[] properties)
+        {
+            try
+            {
+                using (ManagementObjectSearcher searcher = new($"SELECT * FROM {wmiClass}"))
+                {
+                    int deviceCount = 1;
+                    foreach (ManagementObject managmentObject in searcher.Get())
+                    {
+                        var text = $"Устройство {deviceCount++}\n";
+
+                        foreach (string prop in properties)
+                        {
+                            string val = managmentObject[prop]?.ToString() ?? "Нет данных";
+                            text += $"{prop}: {val}\n";
+                        }
+
+                        list.Items.Add(text);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                list.Items.Add($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private void LoadNetworkInfo()
+        {
+            try
+            {
+                int adapterCount = 1;
+                foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+                {
+                    var text = $"Адаптер #{adapterCount++}: {ni.Name}";
+
+                    text += $"Описание: {ni.Description}\n";
+                    text += $"Тип: {ni.NetworkInterfaceType}\n";
+                    text += $"Статус: {ni.OperationalStatus}\n";
+                    text += $"Скорость: {ni.Speed / 1000000}\n";
+
+                    NetworkList.Items.Add(text);
+                }
+            }
+            catch (Exception ex)
+            {
+                NetworkList.Items.Add($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private void LoadInstalledPrograms()
+        {
+            string[] registryPaths =
+            [
+                @"Software\Microsoft\Windows\CurrentVersion\Uninstall",
+                @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+                @"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+            ];
+
+            ReadProgramsFromRoot(Registry.CurrentUser, registryPaths);
+
+            ReadProgramsFromRoot(Registry.LocalMachine, registryPaths);
+        }
+
+        private void ReadProgramsFromRoot(RegistryKey rootKey, string[] paths)
+        {
+            foreach (string path in paths)
+            {
+                using (RegistryKey? key = rootKey.OpenSubKey(path))
+                {
+                    if (key is null) 
+                        continue;
+
+                    foreach (string subkeyName in key.GetSubKeyNames())
+                    {
+                        using (RegistryKey? subkey = key.OpenSubKey(subkeyName))
+                        {
+                            string? displayName = subkey?.GetValue("DisplayName")?.ToString();
+
+                            if (!String.IsNullOrWhiteSpace(displayName) && !ProgramsListBox.Items.Contains(displayName))
+                                ProgramsListBox.Items.Add(displayName);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ syspr/LabWork23/LabWork23/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Drawing;
+
+object locker = new();
+
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.Write("Введите путь к папке с изображениями: ");
+string inputFolder = Console.ReadLine();
+
+Console.Write("Введите путь к папке для сохранения: ");
+string outputFolder = Console.ReadLine();
+
+if (!Directory.Exists(inputFolder))
+{
+    Console.WriteLine("Ошибка: папка c картинуками не существует.");
+    return;
+}
+
+if (!Directory.Exists(outputFolder))
+{
+    Directory.CreateDirectory(outputFolder);
+}
+
+string[] extensions = { ".jpg", ".jpeg", ".png", ".bmp" };
+string[] filePaths = Directory.GetFiles(inputFolder)
+    .Where(file => extensions.Contains(Path.GetExtension(file).ToLower()))
+    .ToArray();
+
+int totalFiles = filePaths.Length;
+
+if (totalFiles == 0)
+{
+    Console.WriteLine("В папке не найдено изображений.");
+    return;
+}
+
+int processedFilesCount = 0;
+
+DrawProgressBar(0, totalFiles);
+
+Parallel.ForEach(filePaths, filePath =>
+{
+    try
+    {
+        using (Bitmap bitmap = new Bitmap(filePath))
+        {
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    Color originalColor = bitmap.GetPixel(x, y);
+                    Color invertedColor = Color.FromArgb(
+                        originalColor.A,
+                        255 - originalColor.R,
+                        255 - originalColor.G,
+                        255 - originalColor.B
+                    );
+                    bitmap.SetPixel(x, y, invertedColor);
+                }
+            }
+
+            string fileName = Path.GetFileName(filePath);
+            string outputPath = Path.Combine(outputFolder, fileName);
+            bitmap.Save(outputPath);
+        }
+    }
+    catch (Exception ex)
+    {
+        lock (locker)
+        {
+            Console.Write(new string(' ', Console.WindowWidth - 1) + "\r");
+            Console.WriteLine($"Ошибка {Path.GetFileName(filePath)}: {ex.Message}");
+        }
+    }
+    finally
+    {
+        int currentProcessed = Interlocked.Increment(ref processedFilesCount);
+
+        lock (locker)
+        {
+            DrawProgressBar(currentProcessed, totalFiles);
+        }
+    }
+});
+
+Console.WriteLine("\nОбработка успешно завершена");
+Console.ReadKey();
+
+void DrawProgressBar(int current, int total)
+{
+    double percentage = (double)current / total * 100;
+    int progressWidth = 30;
+    int filledWidth = (int)(percentage / 100 * progressWidth);
+
+    string bar = new string('#', filledWidth) + new string('-', progressWidth - filledWidth);
+
+    Console.Write($"\r[{bar}] {percentage:F1}%");
+}
